515bc26e1f063a9d92cffc709a9ebb4f2c281a35
Alberto Gonzalez <algonzal@uji.es>
2018-03-15 13:50:11 +0100

    Consignment service now uses a datastore

--- consignment-service/main.go
+++ consignment-service/main.go
@@ -3,67 +3,30 @@ package main
 import (
 	"fmt"
 	"log"
+	"os"
 
 	pb "github.com/agonper/shipper/consignment-service/proto/consignment"
 	vesselProto "github.com/agonper/shipper/vessel-service/proto/vessel"
 	micro "github.com/micro/go-micro"
-	"golang.org/x/net/context"
 )
 
-type Repository interface {
-	Create(*pb.Consignment) (*pb.Consignment, error)
-	GetAll() []*pb.Consignment
-}
-
-type ConsignmentRepository struct {
-	consignments []*pb.Consignment
-}
-
-func (repo *ConsignmentRepository) Create(consignment *pb.Consignment) (*pb.Consignment, error) {
-	updated := append(repo.consignments, consignment)
-	repo.consignments = updated
-	return consignment, nil
-}
-
-func (repo *ConsignmentRepository) GetAll() []*pb.Consignment {
-	return repo.consignments
-}
-
-type service struct {
-	repo         Repository
-	vesselClient vesselProto.VesselServiceClient
-}
-
-func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment, res *pb.Response) error {
-	vesselResponse, err := s.vesselClient.FindAvailable(context.Background(), &vesselProto.Specification{
-		MaxWeight: req.Weight,
-		Capacity:  int32(len(req.Containers)),
-	})
-	log.Printf("Fould vessel: %s \n", vesselResponse.Vessel.Name)
-	if err != nil {
-		return err
-	}
-
-	req.VesselId = vesselResponse.Vessel.Id
-
-	consignment, err := s.repo.Create(req)
-	if err != nil {
-		return err
-	}
-
-	res.Created = true
-	res.Consignment = consignment
-	return nil
-}
-
-func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest, res *pb.Response) error {
-	consignments := s.repo.GetAll()
-	res.Consignments = consignments
-	return nil
-}
+const (
+	defaultDatabaseHost = "localhost:27017"
+)
 
 func main() {
-	repo := &ConsignmentRepository{}
+	databaseHost := os.Getenv("DB_HOST")
+
+	if databaseHost == "" {
+		databaseHost = defaultDatabaseHost
+	}
+
+	session, err := CreateSession(databaseHost)
+	defer session.Close()
+
+	if err != nil {
+		log.Panicf("Could not connect to datastore with host %s - %v", databaseHost, err)
+	}
 
 	srv := micro.NewService(
 		micro.Name("go.micro.srv.consignment"),
@@ -74,7 +37,7 @@ func main() {
 
 	srv.Init()
 
-	pb.RegisterShippingServiceHandler(srv.Server(), &service{repo, vesselClient})
+	pb.RegisterShippingServiceHandler(srv.Server(), &service{session, vesselClient})
 
 	if err := srv.Run(); err != nil {
 		fmt.Println(err)
